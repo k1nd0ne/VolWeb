7be4e4580ed276f5bde56cc186a4e49891151121
k1nd0ne <k1nd0ne@mail.com>
2021-12-19 23:30:49 +0100

    Switching to redis broker due to issues with rabbitmq

--- investigations/celery.py
+++ investigations/celery.py
@@ -2,14 +2,14 @@ from celery import Celery
 import os
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'VolWeb.settings')
 app = Celery('investigations',
-             broker='amqp://admin:mypass@rabbitmq3:5672//',
-             backend='rpc://',
+             broker='redis://redis:6379/0',
+             backend='redis://redis:6379/0',
              include=['investigations.tasks'])
 
 # Optional configuration, see the application user guide.
 app.conf.update(
     result_expires=3600,
 )
-
+app.autodiscover_tasks()
 if __name__ == '__main__':
     app.start()
