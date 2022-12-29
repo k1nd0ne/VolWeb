47ef158d46f8f7f1b6412a12367402bb404e0b72
k1nd0ne <k1nd0ne@mail.com>
2022-12-29 16:41:46 +0100

    VolWeb 1.0.0-beta

--- iocs/admin.py
+++
@@ -1,3 +0,0 @@
-from django.contrib import admin
-from .models import IOC
-admin.site.register(IOC)
--- iocs/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.iocs, name='iocs'),
-    path('new_ioc', views.new_ioc, name='new_ioc'),
-    path('custom_ioc/<int:pk>/', views.custom_ioc, name='custom_ioc'),
-    path('delete_ioc', views.delete_ioc, name='delete_ioc'),
-]
